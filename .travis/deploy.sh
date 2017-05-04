df188cef3c98b03d007cce26bbccfa552bfcb906
Huang YunKun <htynkn@gmail.com>
2017-05-04 10:29:51 +0800

    Update deploy.sh

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ $TRAVIS_PULL_REQUEST == 'false' && $TRAVIS_REPO_SLUG == 'varFamily/cocos-ui-libgdx' && $GDX_VERSION == '1.9.2' && $TRAVIS_BRANCH == 'master' ]];
+if [[ $TRAVIS_PULL_REQUEST == 'false' && $TRAVIS_REPO_SLUG == 'varFamily/cocos-ui-libgdx' && $GDX_VERSION == '1.9.6' && $TRAVIS_BRANCH == 'master' ]];
 then
     echo 'Upload snapshot to maven central'
     ./gradlew uploadArchives
